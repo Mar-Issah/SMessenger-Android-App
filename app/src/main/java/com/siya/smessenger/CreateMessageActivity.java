37f580932b0113f59aa59fed5000bcf9f051d919
Marsiya <67198163+Mar-Issah@users.noreply.github.com>
2021-04-13 15:16:36 -0400

    SMessenger initial commit

---
+++ app/src/main/java/com/siya/smessenger/CreateMessageActivity.java
@@ -0,0 +1,34 @@
+package com.siya.smessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //when you click on the send message button, run this code
+    public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        //intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        //android will call on inbuilt message apps based on the the mime data type
+
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        //startActivity(intent);
+        startActivity(chosenIntent);
+    }
+}
